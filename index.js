12a58df31d8e1e9b6cac47a084f4d857c1dcdb5c
Jennifer Gaillard <jengaillard1@gmail.com>
2019-01-25 03:02:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = 'Grace Hopper';
 const welcomeMessage = 'Welcome to Flatbook, '+currentUser+'!';
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, '+ currentUser.toUpperCase() + '!';
-const shortGreeting = 'Welcome, ';
+const shortGreeting = 'Welcome, '+'G';
